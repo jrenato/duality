bc869d742bc4a99d87d0a596cad839f933a5d01c
karvalho <renato@karvalho.com.br>
2023-07-02 17:15:16 -0300

    Added comments for possible npc shading

--- Characters/npc.gd
+++ Characters/npc.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var quest_giver_component: QuestGiverComponent = %QuestGiverComponent
+#@onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 
 @export var quest_id : int
 @export var active : bool = true
@@ -12,3 +13,11 @@ func _ready() -> void:
 
 func interact_with(player : Player) -> void:
 	quest_giver_component.interact_with(player)
+
+
+#func enter_interaction_area() -> void:
+#	animated_sprite_2d.modulate = Color(Color.WHITE, 0.5)
+
+
+#func exit_interaction_area() -> void:
+#	animated_sprite_2d.modulate = Color(Color.WHITE, 1.0)
